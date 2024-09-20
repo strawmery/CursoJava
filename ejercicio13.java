fcad4575e0ffd22e0427b1b04b7dce322b1170c5
MariaGarcia <mgarciasierragrsr@gmail.com>
2024-09-20 13:50:52 +0200

    ejercicio 14 hay que completar el 13

---
+++ ejercicio13.java
@@ -0,0 +1,8 @@
+public class ejercicio13 {
+    public static void main(String[] args) {
+        System.out.println("lanzando dados...");
+        int dado1 = (int) (Math.random()*6) + 1;
+        int dado2 = (int) (Math.random()*6) + 1;
+        System.out.println("el resultado el dado 1 es "+ dado1 +" y el resultado del dado 2 es "+ dado2);
+    }
+}
---
+++ ejercicio15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ejercicio15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.println("introduzca un numero para verificar si es par o impar: ");
+        num = sc.nextInt();
+        if(num % 2 == 0){
+            System.out.println("el numero es par ");
+        }else{
+            System.out.println("el numero es impar");
+        }
+    }
+}
