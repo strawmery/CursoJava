04854c416c77ac5941c25b2ce1f0a082f8bed740
MariaGarcia <mgarciasierragrsr@gmail.com>
2024-09-20 10:50:09 +0200

    ejercicio 8

--- ejercicio7and8.java
+++ ejercicio7and8.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ejercicio7and8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num,num2,num3;
+        /*int num,num2,num3;
 
         System.out.println("introduzca el primer numero de la operacion: ");
         num = sc.nextInt();
@@ -13,7 +13,14 @@ public class ejercicio7and8 {
         num3 = sc.nextInt();
 
         int sol = num + num2 * num3;
-        System.out.println("la soluciuon de la opracion es: " + sol);
-    
+        System.out.println("la soluciuon de la opracion es: " + sol);*/
+
+        //ejercicio 8 radio de un circulo solicitado por consola
+
+        int radio;
+        System.out.println("introduzca el valor del radio: ");
+        radio = sc.nextInt();
+        double area = Math.PI *  Math.pow(radio,2);
+        System.out.println("el radio es de : " + area);
     }
 }
