0dcc65e3f50a145418a92d03209bf99046347608
MariaGarcia <mgarciasierragrsr@gmail.com>
2024-09-20 10:21:27 +0200

    ejercicio 7 finalizado seguir con el 8 en el mismo archivo

--- ejercicio7and8.java
+++ ejercicio7and8.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ejercicio7and8 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int num,num2,num3;
+
+        System.out.println("introduzca el primer numero de la operacion: ");
+        num = sc.nextInt();
+        System.out.println("introduzca el segundo numero de la operacion: ");
+        num2 = sc.nextInt();
+        System.out.println("introduzca el tercer numero de la operacion: ");
+        num3 = sc.nextInt();
+
+        int sol = num + num2 * num3;
+        System.out.println("la soluciuon de la opracion es: " + sol);
+    
     }
 }
