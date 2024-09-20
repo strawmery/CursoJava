b050de594118ede7913abbe26da01f0bbab66487
MariaGarcia <mgarciasierragrsr@gmail.com>
2024-09-20 13:10:33 +0200

    ejercicio 11

---
+++ ejercicio11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ejercicio11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.println("introduzca un numero: ");
+        num = sc.nextInt();
+        System.out.println("la potencia de tu numero elevado a 2 es igual a: "+ Math.pow(num,2));
+        System.out.println("la raiz cuadrada es: "+ Math.sqrt(num));
+        System.out.println("el valor absoluto de tu numero es: "+ Math.abs(num));
+    }
+}
