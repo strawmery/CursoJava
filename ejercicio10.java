6a8b3e2ba835b0d7af32063c58f2152c1e81e0ba
MariaGarcia <mgarciasierragrsr@gmail.com>
2024-09-20 13:00:16 +0200

    ejercicio 10 terminado

--- ejercicio10.java
+++ ejercicio10.java
@@ -1,3 +1,33 @@
+import javax.swing.JOptionPane;
+
 public class ejercicio10 {
-    
+    public static void main(String[] args) {
+        int num = Integer.parseInt(JOptionPane.showInputDialog("intruduzca un numero: "));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("intruduzca un segundo nombre: "));
+        int calc = Integer.parseInt(JOptionPane.showInputDialog("que operacion desea realizar : 1.suma 2.resta 3.multiplicacion 4.division"));
+        int resul;
+            switch (calc) {
+                case 1:
+                     resul = num + num2;
+                    JOptionPane.showMessageDialog(null,"el resultado es "+resul);
+                    break;
+                case 2:
+                    resul = num - num2;
+                    JOptionPane.showMessageDialog(null,"el resultado es "+resul);
+                    break;
+                case 3:
+                    resul = num * num2;
+                    JOptionPane.showMessageDialog(null,"el resultado es "+resul);
+                    break;  
+                case 4:
+                    resul = num / num2;
+                    JOptionPane.showMessageDialog(null,"el resultado es "+resul);
+                    break;      
+            
+                default:
+                    break;
+            }
+     }
+
 }
+
